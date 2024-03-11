489fde508a6dc507bd50d3a7a55cbd79755959f1
Bholi <najmulbro123@gmail.com>
2024-03-11 21:02:05 +0545

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+print('Welcome to treasure hiding game !')
+row1 = ['❤️', '❤️', '❤️']
+row2 = ['❤️', '❤️', '❤️']
+row3 = ['❤️', '❤️', '❤️']
+map = [row1, row2, row3]
+print(f'{row1}\n{row2}\n{row3}')
+position = input('Where do you want to hide the treasure : ')
+horizontal = int(position[0])
+vertical = int(position[1])
+map[vertical-1][horizontal-1] = 'X'
+print(f'{row1}\n{row2}\n{row3}')
+
+
